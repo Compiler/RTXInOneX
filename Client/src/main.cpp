f63d358b7e34df239c8da17514a1cca8b89047cc
compiler <lukerochedev@gmail.com>
2024-01-14 12:24:42 -0600

    Camera + Viewport + considerations for images.

--- Client/src/main.cpp
+++ Client/src/main.cpp
@@ -3,7 +3,15 @@
 #include <color.h>
 int main(){
 
-    constexpr int imgWidth = 256, imgHeight = 256;
+    int imgWidth = 512;
+    auto aspectRatio = 16.0 / 9.0;
+    int imgHeight = imgWidth / aspectRatio;
+
+    _PrecisionType focalLen = 1.0;
+    Point3 cameraPosition = {0, 0, 0};
+    _PrecisionType vvpHeight = 2.0;
+    _PrecisionType vvpWidth = vvpHeight * vvpWidth / (_PrecisionType)imgHeight;
+
     std::string header = "P3";
     std::string dimensions = std::to_string(imgWidth) + " " + std::to_string(imgHeight);
     std::string colorDepth = "255";
@@ -15,8 +23,8 @@ int main(){
         for(int c = 0; c < imgWidth; c++){
             // For each cell, we need an rgb output
             Color3 curCellColor = {
-                (double) r / imgHeight,
-                (double) c / imgWidth,
+                r < imgHeight / 4 ? 0 : (double) r / imgHeight,
+                c < imgWidth / 4 ? 0 :(double) c / imgWidth,
                 0
             };
             write_color(std::cout, curCellColor);
