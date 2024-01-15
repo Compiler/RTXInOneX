231835e69899e26e424f03ee7c0768aed2d5c2de
Compiler <LukeRocheDev@gmail.com>
2024-01-15 10:13:05 -0600

    Working projection from vvp

--- Client/src/main.cpp
+++ Client/src/main.cpp
@@ -20,20 +20,27 @@ int main(){
     _PrecisionType vvpHeight = 2.0;
     _PrecisionType vvpWidth = vvpHeight * vvpWidth / (_PrecisionType)imgHeight;
 
+    Vec3 vvpU = {vvpWidth, 0, 0};
+    Vec3 vvpV = {0, vvpHeight, 0};
+
+    Vec3 deltaU = vvpU / imgWidth;
+    Vec3 deltaV = vvpV / imgHeight;
+
+    Vec3 vvpTopLeft = cameraPosition - Vec3(0, 0, focalLen) - vvpU/2 - vvpV/2; 
+    Vec3 pixel00 = vvpTopLeft + 0.5*(deltaU + deltaV);
+
     std::string header = "P3";
     std::string dimensions = std::to_string(imgWidth) + " " + std::to_string(imgHeight);
     std::string colorDepth = "255";
 
     std::cout << header << "\n" << dimensions << "\n" << colorDepth << "\n";
 
-    for(int r = 0; r < imgHeight; r++){
-        std::clog << "\rScanlines remaining: " << (imgHeight - r) << ' ' << std::flush;
-        for(int c = 0; c < imgWidth; c++){
+    for(int v = 0; v < imgHeight; v++){
+        std::clog << "\rScanlines remaining: " << (imgHeight - v) << ' ' << std::flush;
+        for(int u = 0; u < imgWidth; u++){
+            Vec3 curPixelCenter = pixel00 + deltaU * u + deltaV * v;
             // For each cell, we need an rgb output
-            Vec3 direction = {
-                static_cast<_PrecisionType>(r),
-                static_cast<_PrecisionType>(c),
-                static_cast<_PrecisionType>(-1)};
+            Vec3 direction = curPixelCenter - cameraPosition;
 
             Ray outputRay = Ray(cameraPosition, direction);
             Color3 resultingPixelColor = ray_color(outputRay);
